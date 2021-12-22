a54ff0215a474b11f03b9ce37e33f4feff55afb7
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-12-22 21:22:30 +0800

    fixed typo

--- src/Components/Nav/hamburger.js
+++ src/Components/Nav/hamburger.js
@@ -38,16 +38,16 @@ export default function Hamburger() {
                         <div className="mx-5">
                             <ul className="z-20">
                                 <li onClick={handleToggle} className="mb-1">
-                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Home</a>
+                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Бид</a>
                                 </li>
                                 <li onClick={handleToggle} className="mb-1">
-                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#feature">Home</a>
+                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#WeWork">Бид юу хийдэг вэ?</a>
                                 </li>
                                 <li onClick={handleToggle} className="mb-1">
-                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#service">Home</a>
+                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Team">Манай хамт олон</a>
                                 </li>
                                 <li onClick={handleToggle} className="mb-1">
-                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#team">home</a>
+                                    <a className="block p-4 text-sm font-semibold text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Project">Төслүүд</a>
                                 </li>
                             </ul>
                         </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -248,9 +248,6 @@ const Projects = () => {
                                 </div>
                             )}
                             </div>
-                            
-                            
-                            
                         </div>
                     </div>
                 </div>
